2c22c206fa0fec20557f101feb2ad8217d839ba1
Bong pheng <phengbong564@gmail.com>
2026-08-26 22:43:35 +0700

    First commit

---
+++ app/src/main/java/kh/com/supheng/jetpack/compose/gitpractice/ui/ScreenHome.kt
@@ -0,0 +1,11 @@
+package kh.com.supheng.jetpack.compose.gitpractice.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ScreenHome(){
+    Text(
+        text = "Hello 1"
+    )
+}
