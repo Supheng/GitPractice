726c894ec05d963043e43d599db4c99f847edd16
Bong pheng <phengbong564@gmail.com>
2026-08-26 23:01:41 +0700

    create new login screen

---
+++ app/src/main/java/kh/com/supheng/jetpack/compose/gitpractice/ui/ScreenLogin.kt
@@ -0,0 +1,9 @@
+package kh.com.supheng.jetpack.compose.gitpractice.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginScreen(){
+    Text(text = "Hello from login screen")
+}
